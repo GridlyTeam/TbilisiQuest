11b97714d0c873b9341f137ea88b68dd27e7077d
Sandro Tvaliashvili <sandrotvaliashvili10@gmail.com>
2026-09-22 15:50:38 +0400

    Point download at the newest build

--- apps/merchant/src/components/Landing.tsx
+++ apps/merchant/src/components/Landing.tsx
@@ -26,7 +26,7 @@ export type PublicStats = {
 }
 
 const APK_URL =
-  'https://expo.dev/artifacts/eas/CfSznMDuiYy3BHvQZ3-8_yGXnQFByOsFEalM91XjmQk.apk'
+  'https://expo.dev/artifacts/eas/IO4x6ChsjvHTShYq_yYtCp4-wcSykr0rdg-pnbkGJFI.apk'
 
 const CONTACT_EMAIL = 'hello@tbilisiquest.ge'
 
